0276ba88b55795d005713d3142b081e1c3858397
Morgoliath <ben.turker.aslan@gmail.com>
2022-02-22 21:17:23 +0300

    Linux command challenges added.

--- Linux/Project-101/Second_Part/user_passwd.sh
+++ Linux/Project-101/Second_Part/user_passwd.sh
@@ -5,49 +5,49 @@
 # The username, password, and host for the account will be displayed.
 
 # Make sure the script is being executed with superuser privileges.
-
+if [[ "${UID}" -ne 0 ]]
+then
+   echo 'Please run this script with sudo or as root.'
+   exit 1
+fi
 
 # Get the username (login).
+read -p 'Enter the username to create: ' USER_NAME
 
 # Get the real name (contents for the description field).
+read -p 'Enter the name of the person or application that will be using this account: ' COMMENT
 
 # Get the password.
+read -sp 'Enter the password to use for the account: ' PASSWORD
 
 # Create the account.
+useradd -c "${COMMENT}" -m ${USER_NAME} 2> /dev/null 
 
 # Check to see if the useradd command succeeded.
 # We don't want to tell the user that an account was created when it hasn't been.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'This username is already exit. Please select different username '
+  exit 1
+fi
 
 # Set the password.
+echo ${PASSWORD} | passwd --stdin ${USER_NAME}
 
 # Check to see if the passwd command succeeded.
+if [[ "${?}" -ne 0 ]]
+then
+  echo 'The password for the account could not be set.'
+  exit 1
+fi
 
 # Force password change on first login.
+passwd -e ${USER_NAME}
 
 # Display the username, password, and the host where the user was created.
-
-!/bin/bash
-
-read -p "Enter your name: " name
-read -p "Enter your username: " username
-egrep "^$username" /etc/passwd >/dev/null
-
-if [[ $? -eq 0 ]]
-then
-        echo -e "\n$username exists"
-        exit
-else
-        read -s -p "Enter your password: " passwd
-        sudo useradd -p $passwd $username
-fi
-
-if [[ $? -eq 0 ]]
-then
-        echo -e "\nUser has been added to the system."
-else
-        echo -e "\nFailed to add to the system."
-fi
-
-sudo passwd -e $username
-
-echo "$username, $passwd," $(whoami)
+echo
+echo 'username:'
+echo "${USER_NAME}"
+echo
+echo 'password:'
+echo "${PASSWORD}"
